9b884c92ee672f90944e20c52fb0e57245c36105
Abraham <abrahamosazee3@gmail.com>
2023-04-25 01:03:51 +0100

    fix error bugs

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,28 +12,22 @@ def display_details(user_id=None):
     except Exception as err:
         print(err.args)
 
-    user_list = json.loads(user_response.text)
-    user_task_list = json.loads(user_tasks.text)
-    for user in user_list:
-        if user['id'] == int(user_id):
-            needed_user = user
-            break
-
+    users = json.loads(user_response.text)
+    user_tasks = json.loads(user_tasks.text)
+    user_list = [user for user in users if user.get("id") == int(user_id)]
     tasks = completed_tasks = 0
-    for user_task in user_task_list:
-        if user_task['userId'] == int(user_id):
+    for user_task in user_tasks:
+        if user_task.get("userId") == int(user_id):
             tasks += 1
-        if (user_task['completed'] is True and
-                user_task['userId'] == int(user_id)):
+        if (user_task.get("completed") is True and
+                user_task.get("userId") == int(user_id)):
             completed_tasks += 1
 
     print('Employee %s is done with tasks(%d/%d):' %
-          (needed_user['name'], completed_tasks, tasks))
-
-    for user_task in user_task_list:
-        if (user_task['completed'] is True and
-                user_task['userId'] == int(user_id)):
-            print('\t{}'.format(user_task['title']))
+          (user_list[0].get("name"), completed_tasks, tasks))
+    [print("\t{}".format(user_task.get("title"))) for user_task in user_tasks
+        if user_task.get("completed") is True
+        and user_task.get("userId") == int(user_id)]
 
 
 if __name__ == "__main__":
